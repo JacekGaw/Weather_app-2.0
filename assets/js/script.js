2c2118eae9eb224a0efa9734a06bed31bca9c8b0
JacekGaw <jac.gawlyta@gmail.com>
2022-01-30 19:09:26 +0100

    Add media queries

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// const appId = 'ddfa6309d6503c702ecfd25217627582'
+
+// fetch("http://api.openweathermap.org/data/2.5/weather?q=Wroclaw&appid=ddfa6309d6503c702ecfd25217627582")
+//     .then(res => res.json())
+//     .then(res => {
+//         console.log(res);
+//     });
+
+// if (window.navigator.geolocation) {
+//     console.log(window.navigator.geolocation.getCurrentPosition(console.log, console.log))
+//     } 
