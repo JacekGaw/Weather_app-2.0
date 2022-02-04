035e3c4c105aa49fccf64a21bfa5ba395d84d2d1
JacekGaw <jac.gawlyta@gmail.com>
2022-02-04 12:46:49 +0100

    Loader and errors output

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 const weatherAppId = 'ddfa6309d6503c702ecfd25217627582';
-const geoCodeAppId = '908338042750688926617x14133';
+const geoCodeAppId = 'a3408200abaa9fc8f66c897db160c3ed';
 const currentLocationPlaceholder = document.querySelector(".current__location");
+const output = document.querySelector(".output__info");
+const input = document.querySelector(".header__search-input");
+const loader = document.querySelector(".output__loader");
 
 
 const body = document.querySelector("main");
@@ -8,7 +11,6 @@ body.style.display = "none";
 
 
 window.onload = (e) => {
-    console.log("ready");
     localStorage.length > 0 && localStorage.City !== undefined ? console.log(localStorage.City) : getUserLocation();
     // getUserLocation();
 }
@@ -24,7 +26,8 @@ const getCords = position => {
 }
 
 const geoCode = async (latitude,longitude) => {
-    const response = await fetch(`https://geocode.xyz/${latitude},${longitude}?json=1&auth=${geoCodeAppId}`)
+    loader.style.display = "block";
+    const response = await fetch(`http://api.positionstack.com/v1/reverse?access_key=${geoCodeAppId}&query=${latitude},${longitude}&output=json&limit=1`, { mode: 'no-cors'})
     .then(response => {
         if(response.ok){
             return response.json();
@@ -33,15 +36,18 @@ const geoCode = async (latitude,longitude) => {
             throw new Error("Something went wrong!");
         }})
     .then(responseJSON => {
-        localStorage['City'] = responseJSON.city;
-        if(responseJSON.city == undefined){
+        console.log(responseJSON.data[0].locality);
+        localStorage['City'] = responseJSON.data[0].locality;
+        if(responseJSON.data[0].locality == undefined){
             throw new Error("Coudn't get your Location");
         }
-        console.log(responseJSON.city)
     })
     .catch(error => {
         console.log(error);
-    })
+        output.innerHTML = `We couldn't get your location. Try type it manualy above!`;
+        input.focus();
+    });
+    loader.style.display = "none";
 }
 
 
