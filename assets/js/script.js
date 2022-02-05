980edecef2f16f4c4a4834be350b84bc71ab1849
JacekGaw <jac.gawlyta@gmail.com>
2022-02-05 11:03:02 +0100

    Update geocode api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 const weatherAppId = 'ddfa6309d6503c702ecfd25217627582';
-const geoCodeAppId = 'a3408200abaa9fc8f66c897db160c3ed';
 const currentLocationPlaceholder = document.querySelector(".current__location");
 const output = document.querySelector(".output__info");
 const input = document.querySelector(".header__search-input");
@@ -12,8 +11,13 @@ body.style.display = "none";
 
 
 window.onload = (e) => {
-    localStorage.length > 0 && localStorage.City !== undefined ? console.log(localStorage.City) : getUserLocation();
-    // getUserLocation();
+    if(localStorage.length > 0 && localStorage.City !== undefined) {
+        console.log(localStorage.City); 
+        fetchWeather(localStorage.City);
+    }
+    else {
+        getUserLocation();
+    }
 }
 
 const getUserLocation = () => {
@@ -28,19 +32,20 @@ const getCords = position => {
 
 const geoCode = async (latitude,longitude) => {
     loader.style.display = "block";
-    const response = await fetch(`http://api.positionstack.com/v1/reverse?access_key=${geoCodeAppId}&query=${latitude},${longitude}&output=json&limit=1`, { mode: 'no-cors'})
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${weatherAppId}`)
     .then(response => {
         if(response.ok){
             return response.json();
         }
         else {
-            throw new Error("Something went wrong!");
+            throw new Error;
         }})
     .then(responseJSON => {
-        console.log(responseJSON.data[0].locality);
-        localStorage['City'] = responseJSON.data[0].locality;
-        if(responseJSON.data[0].locality == undefined){
-            throw new Error("Coudn't get your Location");
+        console.log(responseJSON[0].name);
+        localStorage['City'] = responseJSON[0].name;
+        fetchWeather(responseJSON[0].name);
+        if(responseJSON[0].name == undefined){
+            throw new Error;
         }
     })
     .catch(error => {
@@ -57,7 +62,9 @@ submit.addEventListener('click', () => {
 });
 
 const fetchWeather = async (location) => {
-    const response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${location}&appid=${weatherAppId}`)
+    output.style.display = "none";
+    loader.style.display = "block";
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${weatherAppId}`)
     .then(response => {
         if(response.ok){
             return response.json();
@@ -71,8 +78,12 @@ const fetchWeather = async (location) => {
     })
     .catch(error => {
         console.log(error);
-        output.innerHTML = error;
-    })
+        output.style.display = "block";
+        output.innerHTML = "We couldn't find weather info for this location. Try different one!";
+        input.value = "";
+        input.focus();
+    });
+    loader.style.display = "none";
 }
 
 
